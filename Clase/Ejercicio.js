8cc1498c23d721da866a9df48936cb758d7b7277
204 <204@academicos.cesde.edu.co>
2021-10-16 09:24:00 -0500

    Ejercicios

---
+++ Clase/Ejercicio.js
@@ -0,0 +1,212 @@
+import React from 'react';
+import { Alert,StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+
+export default class Ejercicio extends React.Component {
+
+    constructor(props) {
+        //Defino identificadores como constantes
+        //Numeropi=3.14;
+        super(props);//Para iniciar las constantes
+
+        //Ahora definimos variables
+        this.state = {
+            Valor1:'',
+            Valor2:'',
+            result:''
+        };
+        
+
+        //Crear las funcnones
+        
+    }
+
+    operate = (e,t) => {
+
+        let n1 = parseInt(this.state.Valor1);
+        let n2 = parseInt(this.state.Valor2);
+        let result =0;
+        switch (t) {
+            case 'A':
+                result = n1 && n2 ? n1 + n2 : null;
+                this.setState({result});
+            break;
+
+            case 'S':
+                result = n1 && n2 ? n1 - n2 : null;
+                this.setState({result});
+            break;
+
+            case 'M':
+                result = n1 && n2 ? n1 * n2 : null;
+                this.setState({result});
+            break;
+
+            case 'D':
+                result = n1 && n2 ? n1 / n2 : null;
+                this.setState({result});
+            break;
+
+            default:
+            result = 0;
+            break;
+        }
+    }
+
+/*
+    sumar = () => 
+    {
+        let Valor1 = parseInt(this.state.Valor1);
+        let Valor2 = parseInt(this.state.Valor2);
+        //console.log(Valor1);
+        //console.log(Valor2);
+        const result = Valor1 && Valor2 ? Valor1 + Valor2 : null;
+        console.log(result);
+        this.setState({result});
+        
+    }
+    restar = () => 
+    {
+        let Valor1 = parseInt(this.state.Valor1);
+        let Valor2 = parseInt(this.state.Valor2);
+        //console.log(Valor1);
+        //console.log(Valor2);
+        const result = Valor1 && Valor2 ? Valor1 - Valor2 : null;
+        console.log(result);
+        this.setState({result});
+        
+    }
+    multiplicar = () => 
+    {
+        let Valor1 = parseInt(this.state.Valor1);
+        let Valor2 = parseInt(this.state.Valor2);
+        //console.log(Valor1);
+        //console.log(Valor2);
+        const result = Valor1 && Valor2 ? Valor1 * Valor2 : null;
+        console.log(result);
+        this.setState({result});
+        
+    }
+    dividir = () => 
+    {
+        let Valor1 = parseInt(this.state.Valor1);
+        let Valor2 = parseInt(this.state.Valor2);
+        //console.log(Valor1);
+        //console.log(Valor2);
+        const result = Valor1 && Valor2 ? Valor1 / Valor2 : null;
+        console.log(result);
+        this.setState({result});
+        
+    }
+*/
+    render() {
+        return (
+            <View style={styles.MainContainer}>
+                <Text style={styles.Text}> Calcular Operaciones </Text>
+
+                <Text style={styles.Text}> {this.state.Valor1} </Text>
+
+                <TextInput
+                    placeholder="Numero 1"
+                    onChangeText={(TextInputValue) => this.setState({Valor1:TextInputValue})}
+                    underlineColorAndroid='transparent'
+                    style={styles.TextInputStyleClass}
+                    value={this.state.Valor1}
+                />
+                
+                <Text style={styles.Text}> {this.state.Valor2} </Text>
+                <TextInput
+                    placeholder="Numero 2"
+                    onChangeText={(TextInputValue) => this.setState({Valor2:TextInputValue})}
+                    underlineColorAndroid='transparent'
+                    style={styles.TextInputStyleClass}
+                    value={this.state.Valor2}
+                />
+
+                <TouchableOpacity 
+                activeOpacity={.5} 
+                style={styles.TouchableOpacityStyle} 
+                onPress={e => this.operate(e,'A')}>
+                    <Text style={styles.TextStyle}> Sumar </Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity 
+                activeOpacity={.5} 
+                style={styles.TouchableOpacityStyle} 
+                onPress={e => this.operate(e,'S')}>
+                    <Text style={styles.TextStyle}> Restar </Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity 
+                activeOpacity={.5} 
+                style={styles.TouchableOpacityStyle} 
+                onPress={e => this.operate(e,'M')}>
+                    <Text style={styles.TextStyle}> Multiplicar </Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity 
+                activeOpacity={.5} 
+                style={styles.TouchableOpacityStyle} 
+                onPress={e => this.operate(e,'D')}>
+                    <Text style={styles.TextStyle}> Dividir </Text>
+                </TouchableOpacity>
+
+                <Text style={styles.Text}>Resultado: {this.state.result}</Text>
+
+            </View>
+            );
+        }
+    }
+
+const styles = StyleSheet.create({
+
+    Text: {
+        fontSize: 20, 
+        textAlign: 'center', 
+        marginBottom: 7
+    },
+
+    MainContainer: {
+    
+        alignItems: 'center',
+        flex: 1,
+        paddingTop: 30,
+        backgroundColor: '#fff'
+    
+    },
+    
+    TextInputStyleClass: {
+    
+        textAlign: 'center',
+        width: '90%',
+        marginBottom: 7,
+        height: 40,
+        borderWidth: 1,
+        borderColor: '#FF5722',
+        borderRadius: 5,
+    
+    },
+    
+    TouchableOpacityStyle: {
+    
+        paddingTop: 10,
+        paddingBottom: 10,
+        borderRadius: 5,
+        marginBottom: 7,
+        width: '90%',
+        backgroundColor: '#00BCD4'
+    
+    },
+    
+    TextStyle: {
+        color: '#fff',
+        textAlign: 'center',
+    },
+    
+    rowViewContainer: {
+        fontSize: 20,
+        paddingRight: 10,
+        paddingTop: 10,
+    
+}
+
+});
---
+++ Ejercicio2.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import {StyleSheet, Text, TextInput, View, Button, TouchableOpacity, Alert} from 'react-native';
+
+export default class ValidarNumeros extends Component{
+
+    constructor(props){
+        super(props);
+        this.state ={text:''};
+    }
+
+    ValidarNumeros = (text) => 
+    {
+        if (/^\d+$/.test(text))
+        {
+            this.setState({text:text});
+        }
+    }
+
+    render() {     
+            return (  
+
+            <View style = {MisEstilos.container}>
+                <Text>Operaciones</Text>                     
+                    <TextInput 
+                        keyboardType ='numeric' 
+                        onChangeText= {this.ValidarNumeros}
+                        value={this.state.text} 
+                        placeholder="Solo números" 
+                        underlineColorAndroid='transparent'
+                        style={MisEstilos.ClaseEstilosTextInput}
+                    />
+
+                    <TouchableOpacity
+                    activeOpacity={0.4} 
+                    style={ MisEstilos.EstiloBoton}
+                    onPress={this.Sumar}>
+                    <Text style={MisEstilos.TextoTitulos} >Sumar</Text>
+                    </TouchableOpacity>
+                    
+            </View> 
+            );
+        }
+    }
+const MisEstilos = StyleSheet.create({
+
+    container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    },
+
+    TextoInput:{
+    textAlign: 'center',
+    width: '50%',
+    marginBottom: 7,
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#FF5722',
+    borderRadius: 5,
+    },
+    
+    EstiloBoton:{
+    paddingTop: 10,
+    paddingBottom: 10,
+    borderRadius: 5,
+    marginBottom: 7,
+    width: '50%',
+    backgroundColor: '#00BCD4' 
+    },
+    
+    TextoTitulos:{
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 16
+    },
+
+});
